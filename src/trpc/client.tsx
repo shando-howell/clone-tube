4fa03a15797b793a52c67fa44d0da6f5ca2dec5b
shandohowell <shandohowell94@gmail.com>
2025-03-09 00:43:20 -0500

    Working on configuring TRPC.

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -6,6 +6,8 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
 import { createTRPCReact } from '@trpc/react-query';
 import { useState } from 'react';
+import superjson from 'superjson';
+
 import { makeQueryClient } from './query-client';
 import type { AppRouter } from './routers/_app';
 
@@ -47,7 +49,7 @@ export function TRPCProvider(
     trpc.createClient({
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: superjson,
           url: getUrl(),
         }),
       ],
