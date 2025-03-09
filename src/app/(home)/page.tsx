827c5000f9144ccdd4f4ac3d31eed9e0c0621177
shandohowell <shandohowell94@gmail.com>
2025-03-09 13:57:27 -0500

    Working on categories component.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,24 @@
 import { HydrateClient, trpc } from "@/trpc/server";
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
-import { PageClient } from "./client";
+import { HomeView } from "@/modules/home/ui/views/HomeView";
 
-export default async function Home() {
-  void trpc.hello.prefetch({ text: "Shando" });
+export const dynamic = "force-dynamic";
+
+interface PageProps {
+  searchParams: Promise<{
+    categoryId?: string;
+  }>
+};
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { categoryId } = await searchParams;
+
+  void trpc.categories.getMany.prefetch();
 
   return (
     <HydrateClient>
-      <Suspense fallback={<p>Loading...</p>}>
-        <ErrorBoundary fallback={<p>Error...</p>}>
-          <PageClient />
-        </ErrorBoundary>
-      </Suspense>
+      <HomeView categoryId={categoryId} />
     </HydrateClient>
   );
 }
+
+export default Page;
--- src/app/(home)/protected/page.tsx
+++
@@ -1,7 +0,0 @@
-const Page = () => {
-  return (
-    <div>Sign in to access this page</div>
-  )
-}
-
-export default Page
