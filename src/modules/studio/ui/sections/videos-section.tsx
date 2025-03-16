4189d1c7c1d5bcef81ae8ced61bd8642837e7630
shandohowell <shandohowell94@gmail.com>
2025-03-16 13:01:25 -0500

    Working on studio component.

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -15,6 +15,9 @@ import {
 } from "@/components/ui/table";
 import Link from "next/link";
 import VideoThumbnail from "@/modules/videos/ui/components/video-thumbnail";
+import { snakeCaseToTitle } from "@/lib/utils";
+import { format } from "date-fns";
+import { Globe2Icon, LockIcon } from "lucide-react";
 
 export const VideosSection = () => {
     return (
@@ -23,6 +26,30 @@ export const VideosSection = () => {
                 <VideosSectionSuspense />
             </ErrorBoundary>
         </Suspense>
+    )
+}
+
+const VideoSectionSkeleton = () => {
+    return (
+        <>
+            <div className="border-y">
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="pl-6 w-[510px]">Video</TableHead>
+                            <TableHead>Visibility</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>Date</TableHead>
+                            <TableHead className="text-right">Views</TableHead>
+                            <TableHead className="text-right">Comments</TableHead>
+                            <TableHead className="text-right pr-6">Likes</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                    </TableBody>
+                </Table>
+            </div>
+        </>
     )
 }
 
@@ -55,18 +82,38 @@ const VideosSectionSuspense = () => {
                                     <TableCell>
                                         <div className="flex items-center gap-4">
                                             <div className="relative aspect-video w-36 shrink-0">
-                                                <VideoThumbnail/>
+                                                <VideoThumbnail 
+                                                    imageUrl={video.thumbnailUrl} 
+                                                    previewUrl={video.previewUrl}
+                                                    title={video.title}
+                                                    duration={video.duration || 0}
+                                                />
+                                            </div>
+                                            <div className="flex flex-col overflow-hidden grap-y-1">
+                                                <span className="text-sm line-clamp-1">{video.title}</span>
+                                                <span className="text-xs text-muted-foreground line-clamp-1">
+                                                    {video.description || "No description"}
+                                                </span> 
                                             </div>
                                         </div>
                                     </TableCell>
                                     <TableCell>
-                                        Visibility
+                                        <div className="flex items-center">
+                                            {video.visibility === "private" ? (
+                                                <LockIcon className="size-4 mr-2" />
+                                            ) : (
+                                                <Globe2Icon className="size-4 mr-2" />
+                                            )}
+                                            {snakeCaseToTitle(video.visibility)}
+                                        </div>
                                     </TableCell>
                                     <TableCell>
-                                        Status
+                                        <div className="flex items-center">
+                                            {snakeCaseToTitle(video.muxStatus || "error")}
+                                        </div>
                                     </TableCell>
-                                    <TableCell>
-                                        Date
+                                    <TableCell className="text-sm truncate">
+                                        {format(new Date(video.createdAt), "d MMM yyyy")}
                                     </TableCell>
                                     <TableCell>
                                         Views
--- src/modules/videos/ui/components/video-thumbnail.tsx
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -1,15 +1,42 @@
+import { formatDuration } from "@/lib/utils";
 import Image from "next/image"
 
-const VideoThumbnail = () => {
+interface VideoThumbnailProps {
+  title: string;
+  imageUrl?: string | null;
+  previewUrl?: string | null;
+  duration: number;
+}
+
+const VideoThumbnail = ({
+  title,
+  imageUrl,
+  previewUrl,
+  duration
+}: VideoThumbnailProps) => {
   return (
-    <div className="relative">
+    <div className="relative group">
         {/* Thumbnail wrapper */}
         <div className="relative w-full overflow-hidden rounded-xl aspect-video">
-            <Image src="/placeholder.jpg" alt="Thumbnail" fill className="size-full object-cover"/>
+            <Image 
+              src={imageUrl ?? "/placeholder.jpg"} 
+              alt={title} 
+              fill 
+              className="size-full object-cover group-hover:opacity-0"
+            />
+            <Image 
+              unoptimized={!!previewUrl}
+              src={previewUrl ?? "/placeholder.jpg"} 
+              alt={title} 
+              fill 
+              className="size-full object-cover opacity-0 group-hover:opacity-100"
+            />
         </div>
 
         {/* Video duration box */}
-        {/* TODO: Add video duration box */}
+        <div className="absolute bottom-2 right-2 px-1 py-0.5 rounded bg-black/80 text-white text-xs font-medium">
+          {formatDuration(duration)}
+        </div>
     </div>
   )
 }
