e2b8f58f7dd443f402df4ecc5ca7892fe13a960b
shandohowell <shandohowell94@gmail.com>
2025-03-20 03:12:29 -0500

    Working on thumbnail component.

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -7,6 +7,38 @@ import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 export const videosRouter = createTRPCRouter({
+    restoreThumbnail: protectedProcedure
+        .input(z.object({ id: z.string().uuid()}))
+        .mutation(async ({ ctx, input }) => {
+            const { id: userId } = ctx.user;
+
+            const [existingVideo] = await db
+                .select()
+                .from(videos)
+                .where(and(
+                    eq(videos.id, input.id),
+                    eq(videos.userId, userId),
+                ));
+
+            if (!existingVideo) {
+                throw new TRPCError({ code: "NOT_FOUND" });
+            }
+
+            if (!existingVideo.muxPlaybackId) {
+                throw new TRPCError({ code: "BAD_REQUEST" });
+            }
+
+            const thumbnailUrl = `https://image.mux.com/${existingVideo.muxPlaybackId}/thumbnail.jpg`;
+
+            const [updatedVideo] = await db
+                .update(videos)
+                .set({ thumbnailUrl })
+                .where(and(
+                    eq(videos.id, input.id),
+                    eq(videos.userId, userId)
+                ))
+                .returning();
+        }),
     remove: protectedProcedure
         .input(z.object({ id: z.string().uuid() }))
         .mutation(async ({ ctx, input }) => {
